4a84c831abd9476bea66c0262a7c09b7323ba496
“benigna” <nora.lapyte@gmail.com>
2020-11-20 16:49:31 +0200

    home mytravels ir registration puslapiai

--- src/home.php
+++ src/home.php
@@ -24,6 +24,12 @@
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
+                    <a class="nav-link" href="travels.html">Kelionės</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="mytravels.html">Mano kelionės</a>
+                </li>
+                <li class="nav-item">
                     <a class="nav-link" href="profile.html">Profilis</a>
                 </li>
                 <li class="nav-item">
@@ -33,17 +39,81 @@
                     <a class="nav-link" href="about.html">Apie</a>
                 </li>
             </ul>
-
         </div>
     </nav>
 
-    <div class="container">
+   
 
-        <div class="row justify-content-center">
-            <div class="col-md blokas">Pagrindinis prisijungus</div>
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"/></svg>
+        <div class="container">
+          <div class="carousel-caption text-left">
+            <h1>Figma</h1>
+            <p>Kelionių organizavimo sistema jūsų kolektyvui</p>
+          </div>
         </div>
+      </div>
+      <div class="carousel-item">
+        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"/></svg>
+        <div class="container">
+          <div class="carousel-caption text-left">
+            <h1>Figma</h1>
+            <p>Kelionių organizavimo sistema jūsų kolektyvui</p>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"/></svg>
+        <div class="container">
+          <div class="carousel-caption text-left">
+            <h1>Figma</h1>
+            <p>Kelionių organizavimo sistema jūsų kolektyvui</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
 
-        <footer class="my-5 pt-5 text-muted text-center text-small">
+
+
+  <div class="container blokas home">
+
+    <div class="row">
+      <div class="col-lg-4">
+        <img class="bd-placeholder-img" src="assets/map.svg" width="140" height="140" >
+		<h2>Kelionės</h2>
+		<p><a class="btn" href="#" role="button">Plačiau &raquo;</a><p>
+      </div>
+      <div class="col-lg-4">
+        <img class="bd-placeholder-img" src="assets/compass.svg" width="140" height="140" >
+		<h2>Mano kelionės</h2>
+		<p><a class="btn" href="#" role="button">Plačiau &raquo;</a><p>
+      </div>
+      <div class="col-lg-4">
+        <img class="bd-placeholder-img" src="assets/user.svg" width="140" height="140" >
+		<h2>Profilis</h2>
+		<p><a class="btn" href="#" role="button">Plačiau &raquo;</a><p>
+      </div>
+    </div>
+
+        
+    </div>
+		<footer class="container my-5 pt-5 text-muted text-center text-small">
             <p class="mb-1">&copy; Fairee</p>
             <ul class="list-inline">
                 <li class="list-inline-item"><a href="#">Privacy</a></li>
@@ -51,8 +121,7 @@
                 <li class="list-inline-item"><a href="#">Support</a></li>
             </ul>
         </footer>
-    </div>
-
+		
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
@@ -60,8 +129,8 @@
         integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
         crossorigin="anonymous"></script>
     <script>window.jQuery || document.write('<script src="../assets/js/vendor/jquery.slim.min.js"><\/script>')</script>
-    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="form-validation.js"></script>
+    <script src="assets/dist/js/bootstrap.bundle.min.js"></script>
+   
 </body>
 
 </html>
